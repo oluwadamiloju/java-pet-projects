d568d84cc553b6d8657305617e3cfad4daa9583b
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-15 23:17:31 +0100

    player

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,7 +1,15 @@
 public class MusicPlayer {
     boolean isOn;
+    int numberOfSongs;
 
     public boolean turnOn() {
         return !isOn;
+    }
+
+    public int downloadMusic() {
+        if (turnOn()) {
+            numberOfSongs += 1;
+        }
+        return numberOfSongs;
     }
 }
