adc33d706fde16eedac76e5f90d5f53692458b06
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-19 21:13:48 +0100

    changes

--- test/MusicPlayerTest.java
+++ test/MusicPlayerTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MusicPlayerTests {
+public class MusicPlayerTest {
     private MusicPlayer mp3;
 
     @BeforeEach
@@ -18,12 +18,26 @@ public class MusicPlayerTests {
 
     @Test
     void testThatMusicPlayerCanBeTurnedOn() {
-        assertTrue(mp3.turnOn());
+        assertTrue(!(mp3.isOn));
+    }
+
+    @Test
+    void testThatPowerSwitchWorks() {
+        mp3.powerSwitch();
+        assertTrue(!(mp3.isOn));
+        mp3.powerSwitch();
+        assertFalse(mp3.isOn);
     }
 
     @Test
     void testThatMusicPlayerCanDownloadMusicWhenOn() {
         assertTrue(mp3.turnOn());
         assertEquals(mp3.numberOfSongs + 1, mp3.downloadMusic());
+    }
+
+    @Test
+    void testThatMusicPlayerCannotDownloadMusicWhenOff() {
+        assertFalse(mp3.isOn);
+        assertEquals(mp3.numberOfSongs, mp3.downloadMusic());
     }
 }
