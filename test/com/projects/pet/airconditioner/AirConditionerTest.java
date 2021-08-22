e5248aacca3c3f14863977c47d3e7416a468a10f
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-22 15:08:41 +0100

    ac

---
+++ test/com/projects/pet/airconditioner/AirConditionerTest.java
@@ -0,0 +1,13 @@
+package com.projects.pet.airconditioner;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class AirConditionerTest {
+    private AirConditioner ac;
+
+    @BeforeEach
+    void beforeEach() {
+        ac = new AirConditioner();
+    }
+}
