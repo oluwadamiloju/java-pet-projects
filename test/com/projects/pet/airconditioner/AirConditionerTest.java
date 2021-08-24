f7d3da41a3eb66042483e76f9a7c5fff8c19ad06
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-08-24 22:30:50 +0100

    update

--- test/com/projects/pet/airconditioner/AirConditionerTest.java
+++ test/com/projects/pet/airconditioner/AirConditionerTest.java
@@ -1,7 +1,9 @@
 package com.projects.pet.airconditioner;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AirConditionerTest {
     private AirConditioner ac;
@@ -9,5 +11,57 @@ public class AirConditionerTest {
     @BeforeEach
     void beforeEach() {
         ac = new AirConditioner();
+    }
+
+    @Test
+    void airConditionerComesOnWhenTurnedOn() {
+        assertFalse(ac.isOn());
+        ac.turnOn();
+        assertTrue(ac.isOn());
+    }
+
+    @Test
+    void airConditionerGoesOffWhenTurnedOff() {
+        assertFalse(ac.isOn());
+        ac.turnOn();
+        assertTrue(ac.isOn());
+        ac.turnOff();
+        assertFalse(ac.isOn());
+    }
+
+    @Test
+    void airConditionerCanIncreaseTemperature() {
+        assertFalse(ac.isOn());
+        ac.turnOn();
+        assertTrue(ac.isOn());
+        ac.increaseTemperature(20);
+        assertEquals(20 + 1, ac.getHigherTemperature());
+    }
+
+    @Test
+    void airConditionerCanReduceTemperature() {
+        assertFalse(ac.isOn());
+        ac.turnOn();
+        assertTrue(ac.isOn());
+        ac.reduceTemperature(20);
+        assertEquals(20 - 1, ac.getLowerTemperature());
+    }
+
+    @Test
+    void testThatTemperatureCannotIncreasePastItsHighestLimit() {
+        assertFalse(ac.isOn());
+        ac.turnOn();
+        assertTrue(ac.isOn());
+        ac.increaseTemperature(30);
+        assertEquals(30, ac.getHigherTemperature());
+    }
+
+    @Test
+    void testThatTemperatureCannotReducePastItsHighestLimit() {
+        assertFalse(ac.isOn());
+        ac.turnOn();
+        assertTrue(ac.isOn());
+        ac.reduceTemperature(16);
+        assertEquals(16, ac.getLowerTemperature());
     }
 }
